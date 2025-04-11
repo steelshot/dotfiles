f6e2c897f0c406f6fe2be5bed6eeb45f30d1943f
Džiugas Eiva <dziugaseiva@gmail.com>
2025-04-11 17:29:50 +0100

    Update envvars.zsh to support viddy v1.0.0 and onwards

--- plugin/envvars.zsh
+++ plugin/envvars.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 #
-# Copyright (c) 2024 Džiugas Eiva GPL-3.0-only
+# Copyright (c) 2024, 2025 Džiugas Eiva GPL-3.0-only
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public Licence as published by
@@ -17,4 +17,4 @@
 #
 
 # use viddy (if available) instead of watch for https://github.com/Thearas/zsh-watch plugin
-(( ${+commands[viddy]} )) && export ZSH_WATCH=viddy ZSH_WATCH_FLAGS="-t -d -n1 --pty"
+(( ${+commands[viddy]} )) && export ZSH_WATCH=viddy ZSH_WATCH_FLAGS="-dtws --disable_auto_save"
